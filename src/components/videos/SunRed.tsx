22e72eee9ed96438a351f79d179d008f235a0684
Zeyad Lotfy <zezoramy380@gmail.com>
2024-10-05 20:59:29 +0300

    S

--- src/components/videos/SunRed.tsx
+++ src/components/videos/SunRed.tsx
@@ -1,6 +1,6 @@
 export default function SunRed() {
     return (
-        <video width="600" height="700" autoPlay loop preload="">
+        <video width="600" height="700" autoPlay loop controls >
             <source src="https://interactives.natgeofe.com/high-touch/ngscience-21-the-sun/builds/v4/ngm-assets/video/Comp_3_1200_sm.mp4" type="video/mp4" />
             Your browser does not support the video tag.
         </video>
