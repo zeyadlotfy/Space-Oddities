086688ba29e081192a895cc25ef66ab84ea2f5c1
Zeyad Lotfy <zezoramy380@gmail.com>
2024-10-06 17:35:08 +0300

    D

--- src/app/video/page.tsx
+++ src/app/video/page.tsx
@@ -1,15 +1,7 @@
 
 
-// export default function Page() {
-//     return <video width="600" height="700" autoPlay loop controls preload="none" >
-//         <source src="https://utfs.io/f/IdAErmi3paV8xkBiGvR8K4qWhtcY9XPEl5vF27JANOLUVMxu" type="video/mp4" />
-//         Your browser does not support the video tag.
-//     </video>
-// }
-
-
 import Video from 'next-video';
 
 export default function Page() {
-    return <Video src="https://utfs.io/f/IdAErmi3paV8xkBiGvR8K4qWhtcY9XPEl5vF27JANOLUVMxu" />;
+    return <Video src="https://utfs.io/f/IdAErmi3paV81axqbCspdYDRa0iLO3lpvXFqgH87rmMBj4VJ" />;
 }
